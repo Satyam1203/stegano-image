4bf5176f3a83b92e932d21915a8e5427a3915bea
Vishwas Paikra <vishwaspaikra007@gmail.com>
2022-03-05 01:01:04 +0530

    loading svg and text

--- public/js/socket.js
+++ public/js/socket.js
@@ -3,7 +3,7 @@ let userId = window.crypto.getRandomValues(new Uint32Array(1))[0]
 
 socket.on(userId, data => {
     let loading = document.querySelector('#loading')
-    let para = document.querySelector('#loading pre')
+    let para = document.querySelector('#loading div')
 
     if(data.status == 'video_URL') {
         console.log(data.URL)
@@ -13,15 +13,20 @@ socket.on(userId, data => {
         video.src = data.URL
         video.style.display = "block"
         loading.style.display = "none"
-        para.innerHTML = "Loading" 
+        para.innerHTML = "<p>Loading</p>" 
     } else if(data.status == 'image_URL') {
         let img = document.querySelector('.decode-video label img')
         img.src = data.URL
         img.parentElement.style.display = 'flex'
         loading.style.display = "none"
-        para.innerHTML = "Loading" 
+        para.innerHTML = "<p>Loading</p>" 
+    } else if(data.status == "failed") {
+        console.log(data.msg)
+        alert(data.msg)
+        loading.style.display = "none"
+        para.innerHTML = "<p>Loading</p>" 
     } else {
         console.log(data.msg)
-        para.innerHTML += "\n" + data.msg
+        para.innerHTML += "<p>" + data.msg + "</p>"
     }
 })
